3d27fee53566068f9c61c6310b6da3ea18e4464b
Abhinav Singh <adityasaha890@gmail.com>
2019-01-25 16:43:18 +0530

    f

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var Storage = multer.diskStorage({
 });
 var upload = multer({
     storage: Storage
-}).array("imgUploader",10000).single('file'); //Field name and max count
+}).array("file",10000); //Field name and max count
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
